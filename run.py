800342c53167422a693b999c7e3a30a283af433e
1138-4EB <1138-4EB@users.noreply.github.com>
2019-06-04 19:02:02 +0200

    split Wishbone verification components from main repository

--- run.py
+++ run.py
@@ -14,7 +14,10 @@ ui = VUnit.from_argv()
 ui.add_random()
 ui.add_verification_components()
 lib = ui.library("vunit_lib")
-lib.add_source_files(join(root, "test", "*.vhd"))
+lib.add_source_files([
+    join(root, "src", "*.vhd"),
+    join(root, "test", "*.vhd")
+])
 
 
 def encode(tb_cfg):
@@ -25,7 +28,7 @@ def gen_wb_tests(obj, *args):
     for dat_width, num_cycles, strobe_prob, ack_prob, stall_prob in product(*args):
         tb_cfg = dict(
             dat_width=dat_width,
-            #TODO remove fixed addr
+            #  TODO remove fixed addr
             adr_width=32,
             strobe_prob=strobe_prob,
             ack_prob=ack_prob,
@@ -39,7 +42,7 @@ def gen_wb_tests(obj, *args):
 tb_wishbone_slave = lib.test_bench("tb_wishbone_slave")
 
 for test in tb_wishbone_slave.get_tests():
-    #TODO strobe_prob not implemented in slave tb
+    #  TODO strobe_prob not implemented in slave tb
     gen_wb_tests(test, [8, 32], [1, 64], [1.0], [0.3, 1.0], [0.4, 0.0])
 
 
