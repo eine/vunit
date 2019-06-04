30681c83e11efea95046f87794e7d021b938eaad
1138-4EB <1138-4EB@users.noreply.github.com>
2019-06-04 05:10:00 +0200

    move Wishbone verification components to separate repository

--- vunit/vhdl/verification_components/run.py
+++ vunit/vhdl/verification_components/run.py
@@ -6,47 +6,11 @@
 
 from os.path import join, dirname
 from vunit import VUnit
-from itertools import product
 
-root = dirname(__file__)
 
 ui = VUnit.from_argv()
 ui.add_random()
 ui.add_verification_components()
-lib = ui.library("vunit_lib")
-lib.add_source_files(join(root, "test", "*.vhd"))
-
-
-def encode(tb_cfg):
-    return ",".join(["%s:%s" % (key, str(tb_cfg[key])) for key in tb_cfg])
-
-
-def gen_wb_tests(obj, *args):
-    for dat_width, num_cycles, strobe_prob, ack_prob, stall_prob in product(*args):
-        tb_cfg = dict(
-            dat_width=dat_width,
-            #  TODO remove fixed addr
-            adr_width=32,
-            strobe_prob=strobe_prob,
-            ack_prob=ack_prob,
-            stall_prob=stall_prob,
-            num_cycles=num_cycles)
-        config_name = encode(tb_cfg)
-        obj.add_config(name=config_name,
-                       generics=dict(encoded_tb_cfg=encode(tb_cfg)))
-
-
-tb_wishbone_slave = lib.test_bench("tb_wishbone_slave")
-
-for test in tb_wishbone_slave.get_tests():
-    #  TODO strobe_prob not implemented in slave tb
-    gen_wb_tests(test, [8, 32], [1, 64], [1.0], [0.3, 1.0], [0.4, 0.0])
-
-
-tb_wishbone_master = lib.test_bench("tb_wishbone_master")
-
-for test in tb_wishbone_master.get_tests():
-    gen_wb_tests(test, [8, 32], [1, 64], [0.3, 1.0], [0.3, 1.0], [0.4, 0.0])
-
+ui.library("vunit_lib").add_source_files(join(dirname(__file__), "test", "*.vhd"))
 
 ui.main()
